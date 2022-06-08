9708b4de4c2320349c7ccd7539293b847b2fdc84
binarygit <inscription4@outlook.com>
2022-06-08 15:42:10 +0545

    create custom colorize method for string

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,13 +1,16 @@
 #!/usr/bin/ruby
 require 'debug'
+require_relative 'core_extentions/string/colorize.rb'
 
 class Mastermind
+  using Colorize
+
   def play
     display_main_menu
   end
 
   def display_main_menu
-    puts 'Hello'
+    puts 'Hello'.red
   end
 end
 
